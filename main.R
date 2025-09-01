65a0c8755753bc2ed5ba8080d5b7a951448fbcb7
pdung31 <87981733+pdung31@users.noreply.github.com>
2025-09-01 13:12:05 +0200

    shorten names

--- main.R
+++ main.R
@@ -3,9 +3,9 @@ library(here)
 
 gss_cat
 
-tv_hours_table <- gss_cat |>
+tv_hours_tab <- gss_cat |>
   filter(age < 30) |>
   group_by(marital) |>
   summarise(mean_tv_hours = mean(tvhours, na.rm = TRUE))
 
-write_csv(tv_hours_table, file = here("TV_hours_by_Marital.csv"))
+write_csv(tv_hours_tab, file = here("TV_hours_by_Marital.csv"))
